558b91f2f03bb458eb48c767edbd4086c006d58c
Ayush Jain <ayushjain9501@gmail.com>
2019-06-22 13:41:02 +0530

    mess view

---
+++ src/components/mess/menu.js
@@ -0,0 +1,45 @@
+export const obj = {
+  Hall1: {
+    Sunday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    },
+
+    Monday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    },
+
+    Tuesday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    },
+
+    Wednesday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    },
+
+    Thursday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    },
+
+    Friday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    },
+
+    Saturday: {
+      breakfast: ["dish1", "dish2", "dish3"],
+      lunch: ["dish1", "dish2", "dish3"],
+      dinner: ["dish1", "dish2", "dish3"]
+    }
+  }
+};
