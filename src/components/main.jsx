e78de3433107d01604dc54da610b2c2fe08693f8
Ayush Jain <ayushjain9501@gmail.com>
2019-06-26 15:09:25 +0530

    mess-ui

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import Mess from "./mess/mess";
 
-const Main = props => {
-  return (
-    <div
-      className={
-        "main-wrapper" + (props.sidebarActive ? "" : " sidebar-hidden")
-      }
-    >
-      {/* Depending on page passed in props, render corresponding component */}
-    </div>
-  );
-};
+class Main extends Component {
+  renderPage = props => {
+    if (props.page.name === "Mess") {
+      return <Mess />;
+    }
+  };
+  render() {
+    return (
+      <div
+        className={
+          "main-wrapper mt-2" +
+          (this.props.sidebarActive ? "" : " sidebar-hidden")
+        }
+      >
+        {/* Depending on page passed in props, render corresponding component */}
+        {this.renderPage(this.props)}
+      </div>
+    );
+  }
+}
 
 export default Main;
