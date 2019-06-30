0a27debf14c2dd7a4cb1a336a851e10d5ef48938
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-07-01 00:53:04 +0530

    Added calendar filter & overlay, feed tag chips (#40)

--- src/components/calendar/filterModal.jsx
+++ src/components/calendar/filterModal.jsx
@@ -2,142 +2,172 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Modal,
-  Button,
-  FormGroup,
-  FormControlLabel,
-  Checkbox
+  FormControl,
+  Input,
+  InputLabel,
+  Select,
+  MenuItem,
+  ListItemText,
+  Checkbox,
+  CardContent
 } from "@material-ui/core";
-import Calendar from "./calendar";
+// import Calendar from "./calendar";
 
 const useStyles = makeStyles(theme => ({
-   paper: {
-     flex: 1,
-     position: "absolute",
-     margin: "auto",
-     width: 300,
-     backgroundColor: theme.palette.background.paper,
-     boxShadow: theme.shadows[5],
-     padding: theme.spacing(1),
-     outline: "none"
-   },
-   cardBody: {
-     padding: "0 16px"
-   }
- }));
+  paper: {
+    flex: 1,
+    position: "absolute",
+    margin: "auto",
+    width: 300,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(1),
+    outline: "none"
+  },
+  cardBody: {
+    padding: "0 16px"
+  },
+  modal: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}));
 
- const FilterModal = props => {
-   const classes = useStyles();
-   const [showOnlySubscribed, toggleSubscribed] = React.useState(false);
-   const [selectAll, toggleSelectAll] = React.useState(false);
-   const [tags, changeTags] = React.useState(
-      [
-         {
-            id: 1,
-            title: "tag1",
-            isSubscribed: true,
-            isSelected: false
-         },
-         {
-            id: 2,
-            title: "tag2",
-            isSubscribed: false,
-            isSelected: false
-         }
-      ]
-   );
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250
+    }
+  }
+};
 
-   const handleChange = index => (e, data) => {
-      toggleSelectAll(false);
-      let tempTags = [...tags];
-      tempTags[index].isSelected = data;
-      changeTags(tempTags);
-      props.filter(tempTags);
-   }
+const FilterModal = props => {
+  const classes = useStyles();
+  const tagNames = props.tags.map(tag => tag.name);
+  //   const [selectAll, toggleSelectAll] = React.useState(true);
+  //   const [showOnlySubscribed, toggleSubscribed] = React.useState(false);
 
-   const handleSelectAll = (e, checked) => {
-      toggleSelectAll(checked);
-      console.log(checked);
-      let newTags = [...tags]
-      if(showOnlySubscribed){
-         newTags = tags.map(tag => {
-            if(tag.isSubscribed)
-               return { ...tag, isSelected: checked};
-            else
-               return { ...tag, isSelected: false};
-         });
-      }
-      else{
-         newTags = tags.map(tag => { return {...tag, isSelected: true}})
-      }
-      changeTags(newTags);
-      console.log(newTags);
-      props.filter(newTags);
-   };
-   const selectSubscribe = () => {
-      toggleSubscribed(true);
-      toggleSelectAll(true);
-      changeTags(tags.map(tag => {
-         if(tag.isSubscribed)
-            return { ...tag, isSelected: true};
-         else
-            return { ...tag, isSelected: false};
-         }));
-   };
-   const handleAll = () => { 
-      toggleSubscribed(false);
-      toggleSelectAll(true);
-      changeTags(tags.map(tag => { return {...tag, isSelected: true};}));
-    };
+  //   const handleChange = index => (e, data) => {
+  //      toggleSelectAll(false);
+  //      let tempTags = [...tags];
+  //      tempTags[index].isSelected = data;
+  //      changeTags(tempTags);
+  //      props.filter(tempTags);
+  //   };
 
-   return (
-      <Modal
-         aria-labelledby="simple-modal-title"
-         aria-describedby="simple-modal-description"
-         open={props.open}
-         onClose={props.onClose}
-         style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-         }}
-      >
-         <div className={classes.paper}>
-            <Button
-               onClick={handleAll}
-               variant="contained"
-               color="primary"
-               style={{ width: 105, margin: 10 }}
+  //   const handleSelectAll = (e, checked) => {
+  //      toggleSelectAll(checked);
+  //      console.log(checked);
+  //      let newTags = [...tags];
+  //      if (showOnlySubscribed) {
+  //        newTags = tags.map(tag => {
+  //          if (tag.isSubscribed) return { ...tag, isSelected: checked };
+  //          else return { ...tag, isSelected: false };
+  //        });
+  //      } else {
+  //        newTags = tags.map(tag => {
+  //          return { ...tag, isSelected: true };
+  //        });
+  //      }
+  //      changeTags(newTags);
+  //      console.log(newTags);
+  //      props.filter(newTags);
+  //   };
+  //   const selectSubscribe = () => {
+  //      toggleSubscribed(true);
+  //      toggleSelectAll(true);
+  //      changeTags(
+  //        tags.map(tag => {
+  //          if (tag.isSubscribed) return { ...tag, isSelected: true };
+  //          else return { ...tag, isSelected: false };
+  //        })
+  //      );
+  //   };
+  //   const handleAll = () => {
+  //      toggleSubscribed(false);
+  //      toggleSelectAll(true);
+  //      changeTags(
+  //        tags.map(tag => {
+  //          return { ...tag, isSelected: true };
+  //        })
+  //      );
+  //   };
+
+  return (
+    <Modal open={props.open} onClose={props.onClose} className={classes.modal}>
+      <div className={classes.paper}>
+        <CardContent>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="select-multiple-checkbox">Tags</InputLabel>
+            <Select
+              multiple
+              value={tagNames}
+              onChange={props.filter}
+              input={<Input id="select-multiple-checkbox" />}
+              renderValue={selected => selected.join(" • ")}
+              MenuProps={MenuProps}
             >
-               All
-            </Button>
-            <Button
-               onClick={selectSubscribe}
-               variant="contained"
-               color="primary"
-               style={{ width: 105, margin: 10 }}
-            >
-               Subscribed
-            </Button>
-            
-            <FormGroup row>
-               <FormControlLabel
+              {props.tags.map(tag => (
+                <MenuItem key={tag.tag_id} value={tag.name}>
+                  <Checkbox checked={tag.isSelected} />
+                  <ListItemText primary={tag.name} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </CardContent>
+        {/* <Button
+          onClick={handleAll}
+          variant="contained"
+          color="primary"
+          style={{ width: 105, margin: 10 }}
+        >
+          All
+        </Button>
+        <Button
+          onClick={selectSubscribe}
+          variant="contained"
+          color="primary"
+          style={{ width: 105, margin: 10 }}
+        >
+          Subscribed
+        </Button>
+
+        <FormGroup row>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={selectAll}
+                onChange={handleSelectAll}
+                value={selectAll}
+              />
+            }
+            label="Select All"
+          />
+          {tags
+            .filter(tag => tag.isSubscribed || !showOnlySubscribed)
+            .map((tag, index) => {
+              return (
+                <FormControlLabel
                   control={
-                        <Checkbox checked={selectAll} onChange={handleSelectAll} value={selectAll} />
+                    <Checkbox
+                      checked={tag.isSelected}
+                      onChange={handleChange(index)}
+                      value={tag.title}
+                    />
                   }
-                  label="Select All"
-               />
-               {tags.filter(tag => tag.isSubscribed || !showOnlySubscribed).map((tag, index) => {
-                  return (<FormControlLabel 
-                     control={
-                     <Checkbox checked={tag.isSelected} onChange={handleChange(index)} value={tag.title} />
-                  }
-                     label={tag.title}
-                  />);
-               })}
-            </FormGroup>
-         </div>
-      </Modal>
-   );
+                  label={tag.title}
+                />
+              );
+            })}
+        </FormGroup> */}
+      </div>
+    </Modal>
+  );
 };
 
 export default FilterModal;
--- src/components/feed/feedPost.jsx
+++ src/components/feed/feedPost.jsx
@@ -6,13 +6,31 @@ import {
   CardHeader,
   Button,
   Avatar,
-  Typography
+  Typography,
+  Chip
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import PostModal from "./postModal";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    padding: theme.spacing(0.5)
+  },
+  chip: {
+    margin: theme.spacing(0.5)
+  }
+}));
+
 const FeedPost = props => {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const post = props.post;
+  let dtFormatted = post.event.date.toString();
+  dtFormatted = dtFormatted.slice(0, dtFormatted.indexOf("00:00:00") - 5);
+
   const handleOpen = () => {
     setOpen(true);
   };
@@ -23,19 +41,31 @@ const FeedPost = props => {
     <Card style={{ margin: "10px 10px" }}>
       <CardHeader
         avatar={<Avatar src="avatar.png" />}
-        title={post.title}
-        subheader={post.tag.name}
+        title={post.event.title}
+        subheader={post.event.by}
       />
 
       <CardContent style={{ padding: "0 16px" }}>
         <Typography variant="overline">
           <i className="fa fa-calendar-o" />
-          &nbsp;{post.event.start_time.slice(0,5)}-{post.event.end_time.slice(0,5)} | {post.event.date} | {post.event.venue}
+          &nbsp;{post.event.start_time.slice(0, 5)}-
+          {post.event.end_time.slice(0, 5)} | {dtFormatted} | {post.event.venue}
           <br />
         </Typography>
         <Typography component="p" variant="body2">
           {post.event.summary}
         </Typography>
+        {post.tags.map((tag, index) => {
+          return (
+            <Chip
+              color="primary"
+              size="small"
+              key={index}
+              label={tag.name}
+              className={classes.chip}
+            />
+          );
+        })}
       </CardContent>
 
       <CardActions>
@@ -44,7 +74,12 @@ const FeedPost = props => {
         </Button>
       </CardActions>
 
-      <PostModal open={open} onClose={handleClose} post={post} />
+      <PostModal
+        open={open}
+        onClose={handleClose}
+        post={post}
+        dtFormatted={dtFormatted}
+      />
     </Card>
   );
 };
