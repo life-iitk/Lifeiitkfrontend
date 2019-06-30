b46f46c6b7b9dcbafc7addbc6c6187ccb0ec96bc
Nikhil Agarwal <nikhilag@iitk.ac.in>
2019-06-30 06:42:49 +0530

    Fetch events in calendar

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -7,6 +7,8 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import FeedPost from '../feed/feedPost'
 import ReactDOM from 'react-dom'
+import axios from "axios";
+
 const localizer = momentLocalizer(moment);
 let allViews = Object.keys(Views).map(k => Views[k])
 
@@ -28,44 +30,80 @@ const routing = (
   </Router>
 )
 
+const sampleEvents = {
+  id: 3,
+  title: "Lecture on Bash and Git",
+  start: new Date(2015, 4, 12),
+  end: new Date(2015, 4, 15),
+};
+
 class Calendar extends Component {
     
-  state = {
-    events: [
-      {
-        id: 0,
-        title: 'All Day Event very long title',
-        allDay: true,
-        start: new Date(2015, 3, 0),
-        end: new Date(2015, 3, 1),
-      },
-      {
-        id: 1,
-        title: 'Long Event',
-        start: new Date(2015, 3, 7),
-        end: new Date(2015, 3, 10),
-      },
+  // state = {
+  //   events: [
+  //     {
+  //       id: 0,
+  //       title: 'All Day Event very long title',
+  //       allDay: true,
+  //       start: new Date(2015, 3, 0),
+  //       end: new Date(2015, 3, 1),
+  //     },
+  //     {
+  //       id: 1,
+  //       title: 'Long Event',
+  //       start: new Date(2015, 3, 7),
+  //       end: new Date(2015, 3, 10),
+  //     },
       
-      {
-        id: 2,
-        title: 'DTS STARTS',
-        start: new Date(2016, 2, 13, 0, 0, 0),
-        end: new Date(2016, 2, 20, 0, 0, 0),
-      }
-    ],
-    filterBoxOpen: false
-  };
+  //     {
+  //       id: 2,
+  //       title: 'DTS STARTS',
+  //       start: new Date(2016, 2, 13, 0, 0, 0),
+  //       end: new Date(2016, 2, 20, 0, 0, 0),
+  //     }
+  //   ],
+  //   filterBoxOpen: false
+  // };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }  
+
   openFilterBox = () => {this.setState({filterBoxOpen: true})};
   handleClose = () => {
     this.setState({filterBoxOpen: false});
   }
+  
+  getItems() {
+    axios
+      .get("http://localhost:8000/events/view/month/", { withCredentials: true })
+      .then(res => {
+        const events = res.data;
+        events.forEach(event => {
+          event.id = event.event_id;
+          event.start = event.date;
+          event.end = event.date;
+        })
+        this.setState({ events: events });
+      }
+        )
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <React.Fragment>
         {/* <div> */}
         <div style={{padding: "5px", textAlign: "right"}}>
           <Button
-              onClick={this.openFilterBox}
+              onClick={this.openFilterBox }
               variant="contained"
               color="primary"
               // style={{ width: 105, margin: 10 }}
@@ -73,6 +111,7 @@ class Calendar extends Component {
               Filter
           </Button>
           </div>
+          {console.log(this.state.events)}
         <CalendarBox
           events={this.state.events}
           views={allViews}
