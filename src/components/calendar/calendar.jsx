a645037d065f4c82fcd1b8cdc6835dda9821bfe1
Nilay Majorwar <majorwar@iitk.ac.in>
2019-06-30 08:21:19 +0530

    Added monthwise fetching (#39)
    * Added monthwise fetching
    
    * Monthwise fetching
    
    * Changed to localhost

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,85 +1,117 @@
 import React, { Component } from "react";
 import { Button } from "@material-ui/core";
 import FilterBox from "./filterModal";
-import { Calendar as CalendarBox, Views, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
+import {
+  Calendar as CalendarBox,
+  Views,
+  momentLocalizer
+} from "react-big-calendar";
+import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import FeedPost from '../feed/feedPost'
-import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import FeedPost from "../feed/feedPost";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
 const localizer = momentLocalizer(moment);
-let allViews = Object.keys(Views).map(k => Views[k])
+let allViews = Object.keys(Views).map(k => Views[k]);
 
 const ColoredDateCellWrapper = ({ children }) =>
   React.cloneElement(React.Children.only(children), {
     style: {
-      backgroundColor: 'lightblue',
-    },
-  })
+      backgroundColor: "lightblue"
+    }
+  });
 
-var id_fetched = '';
+var id_fetched = "";
 
 const routing = (
   <Router>
-      <div>
-          <Route path="/:id_fetched" render={props => <FeedPost 
-          post={{event: {title: "Hello",start_time:"12:23:12",end_time:"14:12:12",date:"2019-08-09",venue:"RM101",description:"asad",summary:"wdusi"},tag:{name:"asa",tag_id:"9"}}}></FeedPost>}></Route>      
-      </div>
+    <div>
+      <Route
+        path="/:id_fetched"
+        render={props => (
+          <FeedPost
+            post={{
+              event: {
+                title: "Hello",
+                start_time: "12:23:12",
+                end_time: "14:12:12",
+                date: "2019-08-09",
+                venue: "RM101",
+                description: "asad",
+                summary: "wdusi"
+              },
+              tag: { name: "asa", tag_id: "9" }
+            }}
+          />
+        )}
+      />
+    </div>
   </Router>
-)
+);
+
+const sampleEvents = {
+  id: 3,
+  title: "Lecture on Bash and Git",
+  start: new Date(2015, 4, 12),
+  end: new Date(2015, 4, 15)
+};
 
 class Calendar extends Component {
-    
-  state = {
-    events_all : [
-      {
-        id: 0,
-        title: 'All Day Event very long title',
-        allDay: true,
-        start: new Date(2015, 3, 0),
-        end: new Date(2015, 3, 1),
-        tag_id : 1,
-      },
-      {
-        id: 1,
-        title: 'Long Event',
-        start: new Date(2015, 3, 7),
-        end: new Date(2015, 3, 10),
-        tag_id : 2,
-      },
-      
-      {
-        id: 2,
-        title: 'DTS STARTS',
-        start: new Date(2016, 2, 13, 0, 0, 0),
-        end: new Date(2016, 2, 20, 0, 0, 0),
-        tag_id : 1,
-      },
-      {
-        id: 3,
-        title: 'New',
-        start: new Date(2015, 3, 7),
-        end: new Date(2015, 3,15),
-        tag_id : 2,
-      },
-    ],
-    events: [
-      {
-         id: 0,
-         title: 'All Day Event very long title',
-         allDay: true,
-         start: new Date(2015, 3, 0),
-         end: new Date(2015, 3, 1),
-      },
-    ],
-    filterBoxOpen: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      events_all: [],
+      events: [],
+      date: new Date(),
+      filterBoxOpen: false
+    };
+    // this.onNavigate.bind(this);
+  }
 
+  componentDidMount() {
+    this.getItems();
+  }
+
+  openFilterBox = () => {
+    this.setState({ filterBoxOpen: true });
   };
 
-  openFilterBox = () => {this.setState({filterBoxOpen: true})};
   handleClose = () => {
-    this.setState({filterBoxOpen: false});
+    this.setState({ filterBoxOpen: false });
+  };
+
+  getItems(month, year) {
+    let url = "http://localhost:8000/events/view/month";
+    if (month && year) url += `/p?month=${month}&year=${year}`;
+    axios
+      .get(url, {
+        withCredentials: true
+      })
+      .then(res => {
+        const events = res.data;
+        events.forEach(event => {
+          event.id = event.event_id;
+          event.start = event.date;
+          event.end = event.date;
+        });
+        this.setState({ events_all: events });
+      })
+      .catch(err => console.log(err));
   }
+
+  onNavigate = (newDt, action) => {
+    const currDt = this.state.date;
+    if (
+      currDt.getMonth() !== newDt.getMonth() ||
+      currDt.getFullYear() !== newDt.getFullYear()
+    ) {
+      this.getItems(newDt.getMonth() + 1, newDt.getFullYear());
+    }
+    this.setState({ date: newDt });
+  };
+
   filterTags = (tags) => {
     console.log(tags[0].isSelected)
     console.log(this.state.events_all.length)
@@ -90,35 +122,37 @@ class Calendar extends Component {
   render() {
     return (
       <React.Fragment>
-        {/* <div> */}
-        <div style={{padding: "5px", textAlign: "right"}}>
+        <div style={{ padding: "5px", textAlign: "right" }}>
           <Button
-              onClick={this.openFilterBox}
-              variant="contained"
-              color="primary"
-              // style={{ width: 105, margin: 10 }}
-            >
-              Filter
+            onClick={this.openFilterBox}
+            variant="contained"
+            color="primary"
+          >
+            Filter
           </Button>
-          </div>
-          
+        </div>
+
         <CalendarBox
           events={this.state.events}
           views={allViews}
           step={60}
           showMultiDayTimes
-          defaultDate={new Date(2015, 3, 1)}
+          date={this.state.date}
           components={{
-            timeSlotWrapper: ColoredDateCellWrapper,
+            timeSlotWrapper: ColoredDateCellWrapper
           }}
           localizer={localizer}
-          onSelectEvent={(e, event) => { id_fetched = e['id']; console.log(id_fetched); window.location.replace(id_fetched)}}
-          //onDrillDown={(e,e1) => {console.log(e);console.log(e1);}}
+          onSelectEvent={(e, event) => {
+            id_fetched = e["id"];
+            console.log(id_fetched);
+            window.location.replace(id_fetched);
+          }}
+          onNavigate={this.onNavigate}
         />
         <FilterBox filter={this.filterTags} open={this.state.filterBoxOpen} onClose={this.handleClose}></FilterBox>
       </React.Fragment>
     );
   }
 }
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(routing, document.getElementById("root"));
 export default Calendar;
