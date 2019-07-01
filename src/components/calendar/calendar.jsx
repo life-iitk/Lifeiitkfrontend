6a25fc4cc22d375cbfdf10e59f780ca73d3b627d
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-07-02 03:33:47 +0530

    Patched feed, calendar to use fetched event format

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -21,80 +21,33 @@ const ColoredDateCellWrapper = ({ children }) =>
     }
   });
 
-// const routing = (
-//   <Router>
-//     <div>
-//       <Route
-//         path="/:id_fetched"
-//         render={props => (
-//           <FeedPost
-//             post={{
-//               event: {
-//                 title: "Hello",
-//                 start_time: "12:23:12",
-//                 end_time: "14:12:12",
-//                 date: "2019-08-09",
-//                 venue: "RM101",
-//                 description: "asad",
-//                 summary: "wdusi"
-//               },
-//               tag: { name: "asa", tag_id: "9" }
-//             }}
-//           />
-//         )}
-//       />
-//     </div>
-//   </Router>
-// );
-
 const toCalendarPost = event => {
-  const dtString = event.event.date.toISOString().slice(0, 10);
+  const dtString = event.date.toISOString().slice(0, 10);
   const clPost = {
-    event_id: event.event.event_id,
-    title: event.event.title,
+    event_id: event.event_id,
+    title: event.title,
     // Use opposite time offset to balance toISOString() conversion to UTC
-    start: new Date(`${dtString}T${event.event.start_time}-05:30`),
-    end: new Date(`${dtString}T${event.event.end_time}-05:30`),
-    allDay: event.event.allDay
+    start: new Date(`${dtString}T${event.start_time}-05:30`),
+    end: new Date(`${dtString}T${event.end_time}-05:30`),
+    allDay: event.day_long
   };
   return clPost;
 };
 
-const toFeedPost = event => {
-  const post = {
-    event: {
-      event_id: event.event.event_id,
-      title: event.event.title,
-      by: event.event.by,
-      summary: event.event.summary,
-      date: event.event.date,
-      start_time: event.event.start_time,
-      end_time: event.event.end_time,
-      venue: event.event.venue,
-      allDay: event.event.day_long,
-      description: event.event.description
-    },
-    tags: [...event.tags]
-  };
-  return post;
-};
-
 const fetchedPost = i => {
   return {
-    event: {
-      event_id: i,
-      title: "Lecture on Bash and Git",
-      by: "Programming Club",
-      summary:
-        "This lecture will cover the fundamentals of bash scripting, and will also teach you about the Git version control system.",
-      date: new Date(2019, 7, 15),
-      start_time: "18:30:00",
-      end_time: "22:00:00",
-      venue: "RM101",
-      day_long: false,
-      description:
-        "The topics covered would briefly include introduction to terminal, package managers and the use of Git. It's useful to be familiar with the terminal and the Linux environment for any coding task. Version control tools like Git helps in better flow control and collaboration of code, it is an essential skill."
-    },
+    event_id: 1,
+    title: "Lecture on Bash and Git",
+    summary:
+      "This lecture will cover the fundamentals of bash scripting, and will also teach you about the Git version control system.",
+    date: new Date(2019, 7, 16),
+    by: "Programming Club",
+    start_time: "18:30:00",
+    end_time: "22:00:00",
+    venue: "RM101",
+    day_long: false,
+    description:
+      "The topics covered would briefly include introduction to terminal, package managers and the use of Git. It's useful to be familiar with the terminal and the Linux environment for any coding task. Version control tools like Git helps in better flow control and collaboration of code, it is an essential skill.",
     tags: [
       { name: "PClub", tag_id: 1, description: "SnT Club" },
       { name: "Git", tag_id: 2, description: "Version control system" },
@@ -128,7 +81,7 @@ class Calendar extends Component {
         tag.isSelected = true;
         if (!tags[tag.tag_id]) tags[tag.tag_id] = tag;
       });
-      return toFeedPost(event);
+      return event;
     });
     this.setState({
       allEvents: events,
@@ -199,7 +152,7 @@ class Calendar extends Component {
 
   onEventClick = (e, event) => {
     const selEvent = this.state.allEvents.find(
-      ev => ev.event.event_id === e.event_id
+      ev => ev.event_id === e.event_id
     );
     this.setState({ currPost: selEvent, eventBoxOpen: true });
   };
--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -2,25 +2,25 @@ import React, { Component } from "react";
 import FeedPost from "./feedPost";
 import axios from "axios";
 
-// This sample post shows the format the post objects fetched should be converted to.
-// DONT REMOVE this sample post when no longer necessary. It would probably be
-// useful when making documentation or for someone working here sometime later.
+// Sample post for frontend testing
 const samplePost = {
-  event: {
-    title: "Lecture on Bash and Git",
-    summary:
-      "This lecture will cover the fundamentals of bash scripting, and will also teach you about the Git version control system.",
-    date: "2019-08-16",
-    start_time: "18:30:00",
-    end_time: "22:00:00",
-    venue: "RM101",
-    description:
-      "The topics covered would briefly include introduction to terminal, package managers and the use of Git. It's useful to be familiar with the terminal and the Linux environment for any coding task. Version control tools like Git helps in better flow control and collaboration of code, it is an essential skill."
-  },
-  tag: {
-    name: "Programming Club",
-    tag_id: 1
-  }
+  event_id: 1,
+  title: "Lecture on Bash and Git",
+  summary:
+    "This lecture will cover the fundamentals of bash scripting, and will also teach you about the Git version control system.",
+  date: "2019-08-16",
+  by: "Programming Club",
+  start_time: "18:30:00",
+  end_time: "22:00:00",
+  venue: "RM101",
+  day_long: false,
+  description:
+    "The topics covered would briefly include introduction to terminal, package managers and the use of Git. It's useful to be familiar with the terminal and the Linux environment for any coding task. Version control tools like Git helps in better flow control and collaboration of code, it is an essential skill.",
+  tags: [
+    { name: "PClub", tag_id: 1, description: "SnT Club" },
+    { name: "Git", tag_id: 2, description: "Version control system" },
+    { name: "Bash", tag_id: 3, description: "Default shell for linux" }
+  ]
 };
 
 class Feed extends Component {
