0da223abed08b7551d98e10fdc411d64c1b4924b
Yugesh Ajit Kothari <31157397+yugeshk@users.noreply.github.com>
2019-07-16 17:38:05 -0400

    Update api-config.js

--- src/api-config.js
+++ src/api-config.js
@@ -5,10 +5,10 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === 'lifeiitk.tk') {
     backendHost = 'https://www.lifeiitk.tk';
-} else if (hostname === 'lifiitk.com') {
-    backendHost = 'https://www.lifiitk.com';
+} else if (hostname === 'lifeiitk.com') {
+    backendHost = 'https://www.lifeiitk.com';
 } else {
-    backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
+    backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8000';
 }
 
 export const API_ROOT = `${backendHost}/api`;
