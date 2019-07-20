1299d9cd339056d3e31336a6d084dbcd99dbee19
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-07-21 00:58:42 +0530

    Fixed sunday mess crash

--- src/components/mess/mess.jsx
+++ src/components/mess/mess.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 import MessFilter from "./messFilter";
 import MessContent from "./messContent";
 import menuJSON from "./menu.json";
@@ -6,7 +7,7 @@ import menuJSON from "./menu.json";
 const Mess = props => {
   const menus = menuJSON["menus"];
   const numOfHalls = menus.length;
-  const [day, setDay] = React.useState(new Date().getDay() - 1);
+  const [day, setDay] = React.useState(moment(new Date()).isoWeekday() - 1);
   const [hall, setHall] = React.useState(1);
   return (
     <React.Fragment>
