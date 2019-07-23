7dee57e67ef333d6f11175f8865e5283ad85076d
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-07-24 01:48:49 +0530

    Set up new format for mess menus

--- src/components/mess/mess.jsx
+++ src/components/mess/mess.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import moment from "moment";
-import MessFilter from "./messFilter";
-import MessContent from "./messContent";
-import menuJSON from "./menu.json";
+import React from 'react';
+import moment from 'moment';
+import MessFilter from './messFilter';
+import MessContent from './messContent';
+import messMenus from './messMenus.json';
 
 const Mess = props => {
-  const menus = menuJSON["menus"];
-  const numOfHalls = menus.length;
+  const halls = Object.keys(messMenus);
   const [day, setDay] = React.useState(moment(new Date()).isoWeekday() - 1);
-  const [hall, setHall] = React.useState(1);
+  const [hall, setHall] = React.useState(halls[0]);
   return (
     <React.Fragment>
       <MessFilter
         setHall={setHall}
         setDay={setDay}
-        numOfHalls={numOfHalls}
+        allHalls={halls}
         day={day}
         hall={hall}
       />
-      <MessContent menu={menus[hall - 1][day]} />
+      <MessContent menu={messMenus[hall][day]} />
     </React.Fragment>
   );
 };
